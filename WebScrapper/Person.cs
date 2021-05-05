1b9ea8cbb185fa3e19689c0533d3b3efb7275203
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-05-05 13:16:30 -0500

    fields vs properties

---
+++ WebScrapper/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace WebScrapper
+{
+    public class Person
+    {
+        string _ssn = "SSN";
+        string _passportData = "PassPortData";
+        string _driver = "DriverNumber";
+        string _firstName = "Bethany";
+        string _lastName = "Hampton";
+
+
+        public string FirstName {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (value.Length < 1)
+                {
+                    Console.WriteLine("Input is not accepted. Try again");
+                    return;
+                }
+               
+                 _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (value.Length < 1)
+                {
+                    Console.WriteLine("Input is not accepted. Try again");
+                    return;
+                }
+
+                _lastName = value;
+            }
+        }
+
+        public bool HasDocuments {
+            get
+            {
+                return _ssn.Length > 0 && _passportData.Length > 0 && _driver.Length > 0;
+            }
+
+        }
+    }
+}
---
+++ WebScrapper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebScrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Josh";
+            person.LastName = "Gulledge";
+
+            Console.WriteLine("{0} {1}", person.FirstName, person.LastName);
+            Console.WriteLine(person.HasDocuments);
+
+            person.FirstName = "";
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
